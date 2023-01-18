7d940db60b83a7964885ce7153d90293904b420e
Akash Kumar <aakashsinghbkj@gmail.com>
2023-01-18 10:59:37 +0530

    Third Commit

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class multiply {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 7;
+
+        int multiply1 = a*b;
+
+        System.out.println("Your multiply is " + "  " + " " + multiply1);
+    }
+}
